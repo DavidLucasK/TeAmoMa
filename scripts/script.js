433ee2c847c7e517f3398f759eb2a533e78166f9
David Lucas <david@gorilla.com.br>
2024-08-01 12:09:02 -0300

    Changing localstorage points to endpoint backend real points

--- scripts/script.js
+++ scripts/script.js
@@ -53,11 +53,32 @@ function obterTextos4(lovePoints) {
 const hasVisited = localStorage.getItem('hasVisited');
 
 // Recupera os pontos armazenados e adiciona à variável lovePoints
+const backendUrl = 'https://backendlogindl.vercel.app';
+const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qeHlmbWJwemp5cGlkdWt6bHFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIyNTc5NjIsImV4cCI6MjAzNzgzMzk2Mn0._iRG2YBG6bRkYZG27BRbD-KnrAX1aBHqloTvHGlcNKQ'
+async function fetchUserPoints() {
+    try {
+        const response = await fetch(`${backendUrl}/api/auth/points`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'apiKey': apiKey,
+            },
+        });
 
-//Variavel que pega os LovePoints do LocalStorage
-const storedLovePoints = localStorage.getItem('lovePoints') ? parseInt(localStorage.getItem('lovePoints')) : 0;
+        if (!response.ok) {
+            throw new Error('Erro ao buscar pontos: ' + response.statusText);
+        }
+
+        const data = await response.json();
+        return data.points;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
 //Adiciona aos LovePoints reais da página
-const lovePoints = 500 + storedLovePoints;
+const lovePoints = await fetchUserPoints();
 const points = document.querySelector('.points');
 points.innerHTML = `${lovePoints} LovePoints`;
 

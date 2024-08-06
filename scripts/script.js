77e4bf5ab71c10692f52630bd54438ca754644ff
David Lucas <david@gorilla.com.br>
2024-08-06 19:56:01 -0300

    reverting

--- scripts/script.js
+++ scripts/script.js
@@ -16,9 +16,9 @@ function typeWriter(elemento, texto, callback) {
 const parags = document.querySelectorAll('.home .typewriter');
 
 const textos1 = [
-    'Oi pessoa anônima!',
-    'Essa plataforma foi uma idéia que tive para minha namorada.',
-    'Espero que goste :)',
+    'Oi gatinha!',
+    'Essa plataforma foi uma idéia que tive.',
+    'Acho que você vai gostar :)',
 ];
 
 const textos2 = [
